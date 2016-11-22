3a03fe674e68509740d3677b1f8e5af2ba1dbbc8
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2016-11-22 09:32:11 +0100

    Delete API Key and add Readme

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe
   include HTTParty
 
-  key_value = ENV["FOOD2FORK_KEY"] || "d4cc0c90dc6a94cb47348fa6cf27f44a"
+  key_value = ENV["FOOD2FORK_KEY"]
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api"
   default_params key: key_value
